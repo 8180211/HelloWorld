79a5bed3835bb0b9b520dbfb5243f42ddce7d556
8180211 <marcofernandes465@gmail.com>
2019-05-08 20:49:30 +0100

    segundo commit

--- src/teste/Teste.java
+++ src/teste/Teste.java
@@ -15,8 +15,9 @@ public class Teste {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Hello world!");
+       System.out.println("Ola mundo!");
                 
     }
     
 }
+ 
